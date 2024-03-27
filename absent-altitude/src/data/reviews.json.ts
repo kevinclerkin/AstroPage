aaf2808d2e9c543240b262525fad554a16b4b178
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-03-27 19:59:49 +0000

    Added new pages for about & pricing; added assets & data folders

---
+++ absent-altitude/src/data/reviews.json.ts
@@ -0,0 +1,19 @@
+export const reviews = [{
+    image: import ('../assets/gym-review-1.png'),
+    name: 'Jason',
+    title: 'Founder of Gymshark',
+    text: '“I’ve been working out for years and I’ve never seen such a great gym. It’s the perfect place to get in shape.”',
+    before: '110KG',
+    after: '82KG'
+
+},
+{
+    image: import ('../assets/gym-review-2.png'),
+    name: 'Melissa',
+    title: 'Founder of Gymshark',
+    text: '“Great atmosphere and staff! Love it!”',
+    before: '81KG',
+    after: '55KG'
+
+}, 
+]
